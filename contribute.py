a2fe5d4a9f389ae9034ab1de55521b5e6c4a6bdf
Sasha Shpota <sashashpota@gmail.com>
2019-08-30 22:57:48 +0300

    Optimized, added PEP 8 compatibility

--- contribute.py
+++ contribute.py
@@ -9,8 +9,8 @@ from subprocess import Popen
 
 def main():
     args = arguments()
-    current_datetime = datetime.now()
-    directory = 'repository-' + current_datetime.strftime('%Y-%m-%d-%H-%M-%S')
+    curr_date = datetime.now()
+    directory = 'repository-' + curr_date.strftime('%Y-%m-%d-%H-%M-%S')
     repository = args.repository
     if repository is not None:
         start = repository.rfind('/') + 1
@@ -19,29 +19,34 @@ def main():
     no_weekends = args.no_weekends
     frequency = args.frequency
     os.mkdir(directory)
-    perform(['git', 'init'], directory)
-    start_date = current_datetime.replace(hour=20, minute=0) - timedelta(366)
+    os.chdir(directory)
+    run(['git', 'init'])
+    start_date = curr_date.replace(hour=20, minute=0) - timedelta(366)
     for day in (start_date + timedelta(n) for n in range(366)):
-        if (not no_weekends or day.weekday() < 5) and randint(0, 100) < frequency:
-            for commit_time in (day + timedelta(minutes=m) for m in range(contributions_per_day(args))):
-                contribute(commit_time, directory)
+        if (not no_weekends or day.weekday() < 5) \
+                and randint(0, 100) < frequency:
+            for commit_time in (day + timedelta(minutes=m)
+                                for m in range(contributions_per_day(args))):
+                contribute(commit_time)
 
     if repository is not None:
-        perform(['git', 'remote', 'add', 'origin', repository], directory)
-        perform(['git', 'push', '-u', 'origin', 'master'], directory)
+        run(['git', 'remote', 'add', 'origin', repository])
+        run(['git', 'push', '-u', 'origin', 'master'])
 
-    print('\nRepository generation \x1b[6;30;42mcompleted successfully\x1b[0m!')
+    print('\nRepositorygeneration ' +
+          '\x1b[6;30;42mcompleted successfully\x1b[0m!')
 
 
-def contribute(date, directory):
-    with open(os.path.join(directory, 'README.md'), 'a') as file:
+def contribute(date):
+    with open(os.path.join(os.getcwd(), 'README.md'), 'a') as file:
         file.write(message(date) + '\n\n')
-    perform(['git', 'add', '.'], directory)
-    perform(['git', 'commit', '-m', '"%s"' % message(date), '--date', date.strftime('"%Y-%m-%d %H:%M:%S"')], directory)
+    run(['git', 'add', '.'])
+    run(['git', 'commit', '-m', '"%s"' % message(date),
+         '--date', date.strftime('"%Y-%m-%d %H:%M:%S"')])
 
 
-def perform(commands, context_dir):
-    Popen(commands, cwd=context_dir).wait()
+def run(commands):
+    Popen(commands).wait()
 
 
 def message(date):
@@ -59,20 +64,26 @@ def contributions_per_day(args):
 
 def arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-nw', '--no_weekends', required=False, action='store_true', default=False,
+    parser.add_argument('-nw', '--no_weekends',
+                        required=False, action='store_true', default=False,
                         help="""do not commit on weekends""")
-    parser.add_argument('-mc', '--max_commits', type=int, default=10, required=False,
-                        help="""Defines the maximum amount of commits a day the script can make.
-                        Accepts a number from 1 to 20. If N is specified the script commits
-                        from 1 to N times a day. The exact number of commits is defined randomly
-                        for each day. The default value is 10.""")
-    parser.add_argument('-fr', '--frequency', type=int, default=80, required=False,
-                        help="""Percentage of days when the script performs commits. If N is specified,
-                        the script will commit N%% of days in a year. The default value is 80.""")
+    parser.add_argument('-mc', '--max_commits', type=int, default=10,
+                        required=False, help="""Defines the maximum amount of
+                        commits a day the script can make. Accepts a number
+                        from 1 to 20. If N is specified the script commits
+                        from 1 to N times a day. The exact number of commits
+                        is defined randomly for each day. The default value
+                        is 10.""")
+    parser.add_argument('-fr', '--frequency', type=int, default=80,
+                        required=False, help="""Percentage of days when the
+                        script performs commits. If N is specified, the script
+                        will commit N%% of days in a year. The default value
+                        is 80.""")
     parser.add_argument('-r', '--repository', type=str, required=False,
-                        help="""A link on an empty non-initialized remote git repository. If specified,
-                        the script pushes the changes to the repository. The link is accepted in SSH
-                        or HTTPS format. For example: git@github.com:user/repo.git or
+                        help="""A link on an empty non-initialized remote git
+                        repository. If specified, the script pushes the changes
+                        to the repository. The link is accepted in SSH or HTTPS
+                        format. For example: git@github.com:user/repo.git or
                         https://github.com/user/repo.git""")
     return parser.parse_args()
 
