7418e73fa99bb5e63607e6c8c69febe45bfc6801
Ben <BenjaminHCCarr@users.noreply.github.com>
2021-01-04 14:28:05 -0500

    Change default repo to "main" (#11)

--- contribute.py
+++ contribute.py
@@ -40,7 +40,7 @@ def main():
 
     if repository is not None:
         run(['git', 'remote', 'add', 'origin', repository])
-        run(['git', 'push', '-u', 'origin', 'master'])
+        run(['git', 'push', '-u', 'origin', 'main'])
 
     print('\nRepository generation ' +
           '\x1b[6;30;42mcompleted successfully\x1b[0m!')
