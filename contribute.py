c0aba182382fe36426b3a8162f0028e92f1d6b61
Sergii Prodan <serpro@disroot.org>
2021-01-04 20:21:09 +0100

    Use underscore in option names and rephrase help str

--- contribute.py
+++ contribute.py
@@ -94,12 +94,12 @@ def arguments():
                         to the repository. The link is accepted in SSH or HTTPS
                         format. For example: git@github.com:user/repo.git or
                         https://github.com/user/repo.git""")
-    parser.add_argument('-un', '--user-name', dest='user_name', type=str,
-                        required=False, help="""An optional user.name
-                        to pass to git config of the repository""")
-    parser.add_argument('-ue', '--user-email', dest='user_email', type=str,
-                        required=False, help="""An optional user.email
-                        to pass to git config of the repository""")
+    parser.add_argument('-un', '--user_name', type=str, required=False,
+                        help="""Overrides user.name git config.
+                        If not specified, the global config is used.""")
+    parser.add_argument('-ue', '--user_email', type=str, required=False,
+                        help="""Overrides user.email git config.
+                        If not specified, the global config is used.""")
     return parser.parse_args()
 
 
