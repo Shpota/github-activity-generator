03ac377f941ffb6db6b6c5142bee7c685ca31d8d
Manu Govind <manuvazhunnavar2012@gmail.com>
2021-02-16 01:57:15 +0530

    Small error fix

--- test_contribute.py
+++ test_contribute.py
@@ -17,7 +17,7 @@ class TestContribute(unittest.TestCase):
         self.assertTrue(1 <= contribute.contributions_per_day(args) <= 20)
 
     def test_commits(self):
-        contribute.main(['-nw'])
+        contribute.main(['-nw','--user_name=sampleusername','--user_email=your-username@users.noreply.github.com'])
         self.assertTrue(1 <= int(check_output(['git', 'rev-list', '--count', 'HEAD']).decode('utf-8')) <=20*contribute.NUM_OF_DAYS)
         #git rev-list --count HEAD
 
